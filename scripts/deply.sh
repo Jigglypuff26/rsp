1db3588b62a3336ce8083ca84a0cf9de18556d43
Максим Вековшинин <watchtime24@yandex.ru>
2025-10-03 11:51:49 +0500

    scripts

--- scripts/deply.sh
+++ scripts/deply.sh
@@ -7,11 +7,11 @@ npm run build
 
 # copy to dir
 sudo rm -rf /var/www/rsp/build
-sudo cp -r build /var/www/rsp/
+sudo cp -r ../build /var/www/rsp/
 
 # nginx
 sudo rm -rf /etc/nginx/sites-available/rsp.conf
-sudo cp -r ~/rsp/nginx/. /etc/nginx/sites-available/
+sudo cp -r ../nginx/. /etc/nginx/sites-available/
 sudo rm -rf /etc/nginx/sites-enabled/rsp.conf
 sudo ln -s /etc/nginx/sites-available/rsp.conf /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
