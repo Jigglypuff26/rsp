e77b89e2c09a054190d8d076e931887573514d1e
Максим Вековшинин <watchtime24@yandex.ru>
2025-10-03 20:23:13 +0500

    docker compose ssl

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,14 @@
 git pull
 sudo rm -rf build
 
-# app actions
 npm i
 npm run build
 
-
-# copy to dir
 sudo rm -rf /var/www/rsp/build
-sudo cp -r /home/maksim/rsp/build /var/www/rsp/
+sudo cp -r build /var/www/rsp/
 
-# nginx
 sudo rm -rf /etc/nginx/sites-available/rsp.conf
-sudo cp -r /home/maksim/rsp/nginx/. /etc/nginx/sites-available/
+sudo cp -r nginx/rsp.conf /etc/nginx/sites-available/
 sudo rm -rf /etc/nginx/sites-enabled/rsp.conf
 sudo ln -s /etc/nginx/sites-available/rsp.conf /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
-
-echo "Deploy successfully"
