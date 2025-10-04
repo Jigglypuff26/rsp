e99ac984c14121a1291e8a1cbdeeae86b37fb4fa
Максим Вековшинин <watchtime24@yandex.ru>
2025-10-04 17:56:01 +0500

    docker compose

--- bash.scripts/deploy.sh
+++ bash.scripts/deploy.sh
@@ -21,18 +21,17 @@ sudo rm -rf /var/www/rsp/build
 sudo cp -r build /var/www/rsp/
 
 # работа с конфигом nginx
-./deploy.nginx.conf.sh
-# # удаление (опционально) т.к ещё на этом сервере тестируется другое приложение
-# sudo  rm -f /etc/nginx/sites-enabled/*
+# удаление (опционально) т.к ещё на этом сервере тестируется другое приложение
+sudo  rm -f /etc/nginx/sites-enabled/*
 
-# # удаление старого конфига
-# sudo rm -rf /etc/nginx/sites-available/react.conf
+# удаление старого конфига
+sudo rm -rf /etc/nginx/sites-available/react.conf
 
-# # копирование nginx файла конфигурации
-# sudo cp -r nginx/react.conf /etc/nginx/sites-available/
-# # создание ссылки на nginx файл конфигурации
-# sudo ln -s /etc/nginx/sites-available/react.conf /etc/nginx/sites-enabled/
-# # перезапус nginx
-# sudo systemctl restart nginx
+# копирование nginx файла конфигурации
+sudo cp -r nginx/react.conf /etc/nginx/sites-available/
+# создание ссылки на nginx файл конфигурации
+sudo ln -s /etc/nginx/sites-available/react.conf /etc/nginx/sites-enabled/
+# перезапус nginx
+sudo systemctl restart nginx
 
 echo "Все прошло успешно"
