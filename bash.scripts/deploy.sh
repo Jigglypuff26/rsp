4ebebe9c1ed963ec8b398e693a28467e6e0b1836
Максим Вековшинин <watchtime24@yandex.ru>
2025-10-04 17:17:00 +0500

    docker compose

--- bash.scripts/deploy.sh
+++ bash.scripts/deploy.sh
@@ -18,4 +18,15 @@ sudo rm -rf /var/www/rsp/build
 sudo cp -r build /var/www/rsp/
 
 # работа с конфигом nginx
-bash ./deploy.nginx.conf.sh
+# удаление (опционально) т.к ещё на этом сервере тестируется другое приложение
+sudo  rm -f /etc/nginx/sites-enabled/*
+
+# удаление старого конфига
+sudo rm -rf /etc/nginx/sites-available/react.conf
+
+# копирование nginx файла конфигурации
+sudo cp -r nginx/react.conf /etc/nginx/sites-available/
+# создание ссылки на nginx файл конфигурации
+sudo ln -s /etc/nginx/sites-available/react.conf /etc/nginx/sites-enabled/
+# перезапус nginx
+sudo systemctl restart nginx
