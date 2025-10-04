41a08d2d0ba2e39d491a7609f4a977ca7663391b
Максим Вековшинин <watchtime24@yandex.ru>
2025-10-04 17:34:44 +0500

    docker compose

--- bash.scripts/deploy.sh
+++ bash.scripts/deploy.sh
@@ -3,30 +3,34 @@
 # Пример: bash /home/maksim/rsp/build_scripts/deploy.sh
 
 # подтягиваем изменения
-git pull
-# удаляем старую папку build
-sudo rm -rf build
+# git pull
+# # удаляем старую папку build
+# sudo rm -rf build
 
-# обновляем npm пакеты
-npm i
-# сборка проекта
-npm run build
+# # обновляем npm пакеты
+# npm i
+# # сборка проекта
+# npm run build
 
-# удаляем папку build из репозитория куда смотрит nginx
-sudo rm -rf /var/www/rsp/build
-# копируем папку с собранным проектом в репозитоий куда смотрит nginx
-sudo cp -r build /var/www/rsp/
+# # удаляем папку build из репозитория куда смотрит nginx
+# sudo rm -rf /var/www/rsp/build
+# # копируем папку с собранным проектом в репозитоий куда смотрит nginx
+# sudo cp -r build /var/www/rsp/
 
-# работа с конфигом nginx
-# удаление (опционально) т.к ещё на этом сервере тестируется другое приложение
-sudo  rm -f /etc/nginx/sites-enabled/*
+# # работа с конфигом nginx
+# # удаление (опционально) т.к ещё на этом сервере тестируется другое приложение
+# sudo  rm -f /etc/nginx/sites-enabled/*
 
-# удаление старого конфига
-sudo rm -rf /etc/nginx/sites-available/react.conf
+# # удаление старого конфига
+# sudo rm -rf /etc/nginx/sites-available/react.conf
 
-# копирование nginx файла конфигурации
-sudo cp -r nginx/react.conf /etc/nginx/sites-available/
-# создание ссылки на nginx файл конфигурации
-sudo ln -s /etc/nginx/sites-available/react.conf /etc/nginx/sites-enabled/
-# перезапус nginx
-sudo systemctl restart nginx
+# # копирование nginx файла конфигурации
+# sudo cp -r nginx/react.conf /etc/nginx/sites-available/
+# # создание ссылки на nginx файл конфигурации
+# sudo ln -s /etc/nginx/sites-available/react.conf /etc/nginx/sites-enabled/
+# # перезапус nginx
+# sudo systemctl restart nginx
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+echo parent_path
