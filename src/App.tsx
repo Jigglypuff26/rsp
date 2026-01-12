2cd077f615b426c91a5cfb50e8fc1e3d4c6a3f2e
Максим Вековшинин <watchtime24@yandex.ru>
2026-01-12 11:50:38 +0500

    настройка eslinter + pritter

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          правки +++++ docker compose dev
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>правки +++++ docker compose dev</p>
+    </header>
+  </div>
+);
 
 export default App;
