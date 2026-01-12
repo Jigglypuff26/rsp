1a2c1fc57d1b214820939d1da8e832c833a5ff34
Максим Вековшинин <watchtime24@yandex.ru>
2026-01-13 00:28:33 +0500

    новые правила для pritter

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 
 import { App } from './app/App';
 import { AppProvider } from './app/providers/AppProvider';
-
 import './shared/config/styles/index.css';
-
 import { reportWebVitals } from './shared/lib';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
