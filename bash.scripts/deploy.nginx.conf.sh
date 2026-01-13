029a4e2c4bffdd1fb5b2a23843eae37a8a41a047
Максим Вековшинин <watchtime24@yandex.ru>
2026-01-13 11:45:08 +0500

    настройка docker + nginx

--- bash.scripts/deploy.nginx.conf.sh
+++ bash.scripts/deploy.nginx.conf.sh
@@ -6,15 +6,15 @@
 sudo  rm -f /etc/nginx/sites-enabled/*
 
 # Безопасное удаление только нашего конфига
-sudo rm -f /etc/nginx/sites-enabled/next.conf
+sudo rm -f /etc/nginx/sites-enabled/react.conf
 
 # удаление старого конфига
-sudo rm -rf /etc/nginx/sites-available/next.conf
+sudo rm -rf /etc/nginx/sites-available/react.conf
 
 # копирование nginx файла конфигурации
-sudo cp -r nginx/next.conf /etc/nginx/sites-available/
+sudo cp -r nginx/react.conf /etc/nginx/sites-available/
 # создание ссылки на nginx файл конфигурации
-sudo ln -s /etc/nginx/sites-available/next.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/react.conf /etc/nginx/sites-enabled/
 
 # копировние глобального конфига nginx
 sudo cp -r ./nginx.conf /etc/nginx/nginx.conf
