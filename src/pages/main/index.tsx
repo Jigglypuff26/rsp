10e076e3849fd9cc126ef19c5deff6252e65fbe7
Максим Вековшинин <watchtime24@yandex.ru>
2026-01-13 11:57:16 +0500

    настройка docker + nginx

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -7,7 +7,7 @@ export const MainPage = () => (
   <div className="main-page">
     <header className="main-page__header">
       <img src={logo} className="main-page__logo" alt="logo" />
-      <p>правки +++++ docker compose dev 2026 ssss</p>
+      <p>правки +++ docker compose dev 2026 nginx +++</p>
     </header>
   </div>
 );
